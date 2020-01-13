0552b9dc70d52d7fb7113b24ba6b018b6f74cc0d
D. Joe <deejoe+publicgit@etrumeus.com>
2020-01-13 01:06:53 -0500

    Reset ofcourse contents for Spring 2020, term 2195

--- ofcourse-content/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-setup(name='thecourse',
-      version='1.0',
-      description='courseware on openshift',
-      author='Dr. Professor',
-      author_email='dr@professor.com',
-      url='http://www.python.org/sigs/distutils-sig/',
-      install_requires=['ofcourse>=0.1.12'],
-     )
